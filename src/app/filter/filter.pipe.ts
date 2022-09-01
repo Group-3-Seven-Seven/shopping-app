aec76ebe168f5e3de4ae9664d45fabdfb1425f78
jcruzem <109934669+jcruzem@users.noreply.github.com>
2022-09-02 03:14:04 +0800

    Update filter.pipe.ts

--- src/app/filter/filter.pipe.ts
+++ src/app/filter/filter.pipe.ts
@@ -5,13 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any[], filterString: string, propertyName: string): any[] {
+ transform(value: any[], filterString: string, args: string[]): any[] {
     const result: any = [];
-    if(!value || filterString === '' || propertyName === ''){
+    const propertyName = args[0];
+    const propertyCategory = args[1];
+    
+    if(!value || filterString === '' || propertyName === '' || propertyCategory === ''){
       return value;
     }
     value.forEach((a:any) => {
       if(a[propertyName].trim().toLowerCase().includes(filterString.toLowerCase())){
+        result.push(a);
+      }
+      if(a[propertyCategory].trim().toLowerCase().includes(filterString.toLowerCase())){
         result.push(a);
       }
     })
